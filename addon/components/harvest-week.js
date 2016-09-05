41600332c714aa1a5e21cdb15128eed72df7207b
Jordan Hawker <hawker.jordan@gmail.com>
2016-09-05 13:07:15 -0700

    Add harvest-calendar-view
    Move months to util

--- addon/components/harvest-week.js
+++ addon/components/harvest-week.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
+import Component from 'ember-component';
 import layout from '../templates/components/harvest-week';
 
-export default Ember.Component.extend({
+export default Component.extend({
   layout,
   classNames: ['harvest-week'],
   tagName: 'tr',
---
+++ app/components/harvest-calendar-view.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-harvest/components/harvest-calendar-view';
